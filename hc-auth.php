e243057f1e651c80a0d3f0d573e3a050a5674400
Eric Knappe <eknappe@mla.org>
2018-02-15 11:25:14 -0500

    Created with wp cli scaffold plugin.

---
+++ hc-auth.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Hc Auth
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     hc-auth
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Hc_Auth
+ */
